188557caf905b8724d566768e9f24a9acb87f430
Sreepathi Pai <sree@buckyball>
2019-09-14 23:57:55 -0400

    Bugfix

--- harmonv/harmonv/ptx/_build_ast.py
+++ harmonv/harmonv/ptx/_build_ast.py
@@ -5,13 +5,13 @@
 #
 # Sreepathi Pai
 
-if __name__ == "__main":
+if __name__ == "__main__":
     import argparse
     from _ast_gen import ASTCodeGenerator
 
     p = argparse.ArgumentParser(description="Generate AST classes for PTX")
-    p.add_argument("cfg", help="CFG file", default="_ptx_ast.cfg")
-    p.add_argument("output", help="Output file", default="ptx_ast.py")
+    p.add_argument("cfg", nargs="?", help="CFG file", default="_ptx_ast.cfg")
+    p.add_argument("output", nargs="?", help="Output file", default="ptx_ast.py")
     args = p.parse_args()
 
     ast_gen = ASTCodeGenerator(args.cfg)
